c1d44b6bbaa9170ac2d7d0f050dbee93d4b147a1
Blake Gibbs <bg273376@falmouth.ac.uk>
2024-12-31 23:14:04 +0000

    known issues with outline effects

--- 8BitProject/Assets/Scripts/Player/PlayerInventory.cs
+++ 8BitProject/Assets/Scripts/Player/PlayerInventory.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerInventory : MonoBehaviour
 {
     public int currentMoney;
+    public TMP_Text coinCounter;
 
     public void AddMoney(int value)
     {
         currentMoney += value;
+        coinCounter.text = currentMoney.ToString();
     }
 
     public void RemoveMoney(int value)
